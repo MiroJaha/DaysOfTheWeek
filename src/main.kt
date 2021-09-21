a2d9aa53d9a9f7814fdf7241e1cf3f8284a0e393
Mohammad Jaha <miro.tune@gmail.com>
2021-09-21 11:43:40 +0300

    Uploading Ass

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    val daysOfTheWeek= mapOf(1 to "Saturday",2 to "Sunday",3 to "Monday",4 to "Tuesday",5 to "Wednesday"
+    ,6 to "Thursday",7 to "Friday")
+
+    println("The Days are:")
+    for(days in daysOfTheWeek)
+        println(" $days")
+}
